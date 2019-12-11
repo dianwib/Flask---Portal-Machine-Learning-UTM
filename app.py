ab9e85ba5438abbb1844989eb0091e9f5e28b3dd
dianwib <2nd.wibowo@gmail.com>
2019-12-11 11:56:26 +0700

    11/12

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from keras.utils import to_categorical
+from keras import backend as K
+K.clear_session()
 
 
 
@@ -411,8 +413,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 from sklearn.model_selection import train_test_split
-from keras import backend as K
-K.clear_session()
+
 
 @app.route('/cnn', methods=['GET', 'POST'])
 def show_cnn():
